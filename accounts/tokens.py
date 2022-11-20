061f4fb1d75eca1a6fa75438119ca2b5493d01a3
steqa <5.steqa@gmail.com>
2022-11-20 14:49:44 +0700

    Add the token lifetime when checking it
    Added the ability to pass a parameter containing the allowed token
    lifetime in seconds to the token check function.
    During check, the time elapsed since the token was created will be
    compared with the time specified in this parameter. If the elapsed
    time exceeds the specified parameter, the check returns False.
    This parameter is optional and by default is equal to the value
    specified in the PASSWORD_RESET_TIMEOUT variable

--- accounts/tokens.py
+++ accounts/tokens.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.utils.crypto import constant_time_compare
+from django.utils.http import base36_to_int
+
 import six
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from .models import User
@@ -7,5 +11,38 @@ class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user: User, timestamp: int) -> str:
         return six.text_type(user.pk) + six.text_type(timestamp) + six.text_type(user.is_email_verified)
     
+    def check_token(self, user, token, token_lifetime_in_sec=settings.PASSWORD_RESET_TIMEOUT):
+        """
+        Check that a password reset token is correct for a given user.
+        """
+        if not (user and token):
+            return False
+        # Parse the token
+        try:
+            ts_b36, _ = token.split("-")
+        except ValueError:
+            return False
+
+        try:
+            ts = base36_to_int(ts_b36)
+        except ValueError:
+            return False
+
+        # Check that the timestamp/uid has not been tampered with
+        for secret in [self.secret, *self.secret_fallbacks]:
+            if constant_time_compare(
+                self._make_token_with_timestamp(user, ts, secret),
+                token,
+            ):
+                break
+        else:
+            return False
+
+        # Check the timestamp is within limit.
+        if (self._num_seconds(self._now()) - ts) > token_lifetime_in_sec:
+            return False
+
+        return True
+
     
 account_activation_token = AccountActivationTokenGenerator()
